819879a8b498e4840095d86f3d0ad3048f7deadf
jebalseroo <jebalseroo@unal.edu.co>
2024-01-25 23:29:47 -0500

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Ai7IXYT4CY/JLiDia9T3fdb2c4dAnKeD2MvuIbC+Ez0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ktRpe3e18NpGfTPdryYUEsZpUAa2nA1/CFCQSVU+1nc=\"}"
